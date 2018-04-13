1ed02279c37dd8d4eb9549eaa555cf8682db0835
microlws <microlws@gmail.com>
2018-04-13 09:56:43 +0200

    added search bar component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SearchBar from "./components/search_bar";
 
-const API_KEY = 'AIzaSyClSLNCeriJeo5hNFEdLMZDOBckImOXjmc';
+const API_KEY = "AIzaSyClSLNCeriJeo5hNFEdLMZDOBckImOXjmc";
 
 const App = () => {
-  return <div>HI!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector(".container"));
